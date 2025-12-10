28212966b75798fabd2d5e86e0e19b6f7c4888e6
Harsh Gupta <134432719+harsh232gupta@users.noreply.github.com>
2025-12-10 15:56:36 +0530

    Modify signupValidation to add optional fields
    Updated the signup validation schema to include optional phone and telegram fields.

--- backend/Middlewares/AuthValidation.js
+++ backend/Middlewares/AuthValidation.js
@@ -1,22 +1,6 @@
 
 const Joi = require('joi');
 
-// const signupValidation = (req, res, next) => {
-//     const schema = Joi.object({
-//         name: Joi.string().min(3).max(100).required(),
-//         email: Joi.string().email().required(),
-//         password: Joi.string().min(4).max(100).required()
-//     });
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//         return res.status(400)
-//             .json({ message: "Bad request", error })
-//     }
-//     next();
-// }
-
-
-
 
 const signupValidation = (req, res, next) => {
     const schema = Joi.object({
@@ -24,7 +8,7 @@ const signupValidation = (req, res, next) => {
         email: Joi.string().email().required(),
         password: Joi.string().min(4).max(100).required(),
         gender: Joi.string().valid("Male", "Female", "Other").required(),
-        phone: Joi.string().pattern(/^\d{10}$/).optional(),
+        phone: Joi.string().optional(),
         telegram: Joi.string().max(50).optional() // Optional field
     });
 
